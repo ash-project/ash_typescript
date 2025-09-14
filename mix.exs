3b4f5cb12ab92c410fe1edb1e826f9377f249943
Steffen Deusch <steffen@deusch.me>
2025-09-14 06:18:12 +0200

    docs: add makeup_syntect for js/ts highlighting (#3)

--- mix.exs
+++ mix.exs
@@ -127,6 +127,7 @@ defmodule AshTypescript.MixProject do
       {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:igniter, "~> 0.5", only: [:dev, :test]},
       {:ex_doc, "~> 0.37", only: [:dev, :test], runtime: false},
+      {:makeup_syntect, "~> 0.1", only: [:dev, :test], runtime: false},
       {:sobelow, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:picosat_elixir, "~> 0.2", only: [:dev, :test]},
       {:mix_audit, ">= 0.0.0", only: [:dev, :test], runtime: false},
