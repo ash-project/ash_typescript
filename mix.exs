57db11c5c76ece4740c7823d0e5c6277e3f74460
Torkild G. Kjevik <torkild.kjevik@boitano.no>
2026-03-16 03:51:28 +0100

    chore(deps): use local ash for automagic branch development

--- mix.exs
+++ mix.exs
@@ -176,7 +176,8 @@ defmodule AshTypescript.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ash, "~> 3.20"},
+      # {:ash, git: "https://github.com/ash-project/ash", branch: "automagic", override: true},
+      {:ash, path: "../ash", override: true},
       {:ash_phoenix, "~> 2.0"},
       {:ash_postgres, "~> 2.0", only: [:dev, :test]},
       {:git_ops, "~> 2.0", only: [:dev], runtime: false},
